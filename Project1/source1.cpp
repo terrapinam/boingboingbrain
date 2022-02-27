11aded6cf66e9307cbc76f0bde3314e7ab4fa42b
terrapinam <terrapinam77@gmail.com>
2022-02-27 18:53:27 +0900

    문자열 비교.. (함수x)

--- Project1/source1.cpp
+++ Project1/source1.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
-#include <cmath>
+//#include <cmath>
+#include <cstring>
+#include <string>
 
 int main() {
-    int A, B;
     using std::cin;
     using std::cout;
     using std::endl;
-
-    cin >> A >> B;
-    int result[3];
-    //result[0] = A*(B%10);
-    //result[1] = A*((B/10)%10);
-    //result[2] = A*((B/100)%10);
-    //int sum = result[0]+result[1]*10+result[2]*100;
-    int sum = 0;
-
-    for (int i = 0; i < 3; i++) {
-        result[i] = A * ((B / (int)std::pow(10, i)) % 10);
-        sum += result[i] * (int)std::pow(10, i);
+    char id[50] = "joonas";
+    char A[50];
+    cin >> A;
+    int num = strlen(A);
+    for (int i = 0; i < num; ) {
+        if (A[i] != id[i])
+            break;
+        cout << id[i];
+        if (++i == num)
+            cout << "\?\?\!";
     }
-    cout << result[0] << endl << result[1] << endl << result[2] << endl << sum << endl;
-    return 0;
 }
