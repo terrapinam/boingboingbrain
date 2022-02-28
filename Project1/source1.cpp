962fd1de9a2457c2d48de567abbee52761873d52
terrapinam <terrapinam77@gmail.com>
2022-02-28 21:54:33 +0900

    if문 비교

--- Project1/source1.cpp
+++ Project1/source1.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 //#include <cmath>
-#include <cstring>
-#include <string>
+//#include <cstring>
+//#include <string>
 
 int main() {
     using std::cin;
     using std::cout;
     using std::endl;
-    char id[50] = "joonas";
-    char A[50];
-    cin >> A;
-    int num = strlen(A);
-    for (int i = 0; i < num; ) {
-        if (A[i] != id[i])
-            break;
-        cout << id[i];
-        if (++i == num)
-            cout << "\?\?\!";
+    int A, B;
+    scanf_s("%d %d", &A, &B);
+    if (A > B) {
+        printf(">");
     }
+    else if (A < B) {
+        printf("<");
+    }
+    else if (A == B) {
+        printf("==");
+        }
+    return 0;
 }
