14e44181a0dce5a49b31100d9c61d97c6cd04331
terrapinam <terrapinam77@gmail.com>
2022-03-01 20:26:59 +0900

    switch문으로 성적표
    10을 나눠서 분포도 나눔. case에 비교함수 입력은 안되더라..

--- Project1/source1.cpp
+++ Project1/source1.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
+#include <stdio.h>
 //#include <cmath>
 //#include <cstring>
 //#include <string>
 
 int main() {
-    using std::cin;
-    using std::cout;
-    using std::endl;
-    int A, B;
-    scanf_s("%d %d", &A, &B);
-    if (A > B) {
-        printf(">");
-    }
-    else if (A < B) {
-        printf("<");
-    }
-    else if (A == B) {
-        printf("==");
-        }
-    return 0;
+	using std::cin;
+	using std::cout;
+	using std::endl;
+	int num;
+	cin >> num;
+	if (0 <= num && num <= 100)
+		switch (num / 10)
+		{
+		case 10:
+		case 9:
+			cout << 'A';
+			break;
+		case 8:
+			cout << 'B';
+			break;
+		case 7:
+			cout << 'C';
+			break;
+		case 6:
+			cout << 'D';
+			break;
+		default:
+			cout << 'F';
+			break;
+		}
+	return 0;
 }
