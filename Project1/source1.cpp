65ecd8f64af400ffaabcc4d3116c9362d7ab6be7
terrapinam <terrapinam77@gmail.com>
2022-03-11 23:54:56 +0900

    생각해보니 if문 쓸 필요가 없네....
    오븐타이머 소스 수정(if문 삭제)

--- Project1/source1.cpp
+++ Project1/source1.cpp
@@ -4,11 +4,8 @@ int main() {
 	int A, B, C;
 	std::cin >> A >> B >> C;
 	B += C;
-	if (B > 59)
-	{
-		A += (B / 60);
-		B %= 60;
-	}
+	A += (B / 60);
+	B %= 60;
 	A %= 24;
 	std::cout << A << " " << B;
 }
