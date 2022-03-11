da2463a19536470de2410f3cbf8ca47e1467989a
terrapinam <terrapinam77@gmail.com>
2022-03-11 23:48:49 +0900

    종료시간 알려주기(오븐타이머)
    시간(A시B분)을 받고, 걸리는 시간 C분(minutes)을 입력 받으면
    C분이 지나면 나타날 시각을 출력해줌.
    12:00에 100분 걸리는 요리를 했으면 13:40에 종료됨을 알려줌.

--- Project1/source1.cpp
+++ Project1/source1.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 
 int main() {
-    int h, m;
-    std::cin >> h >> m ;
-    m += 15;
-    if (m >= 60)
-        m -= 60;
-    else
-        h -= 1;
-    if (h >= 24)
-        h -= 24;
-    if (h < 0)
-        h += 24;
-    std::cout << h << " " << m;
-    return 0;
+	int A, B, C;
+	std::cin >> A >> B >> C;
+	B += C;
+	if (B > 59)
+	{
+		A += (B / 60);
+		B %= 60;
+	}
+	A %= 24;
+	std::cout << A << " " << B;
 }
