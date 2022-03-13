7c9a13735f6da222e7d8b05997d7d6e814f3afa6
terrapinam <terrapinam77@gmail.com>
2022-03-13 21:30:37 +0900

    주사위 3개,2개 일치할 때 결과, 모두 다를땐 최댓값 구하기
    문제: 1에서부터 6까지의 눈을 가진 3개의 주사위를 던져서 다음과 같은 규칙에 따라 상금을 받는 게임이 있다.
    
    같은 눈이 3개가 나오면 10,000원+(같은 눈)×1,000원의 상금을 받게 된다.
    같은 눈이 2개만 나오는 경우에는 1,000원+(같은 눈)×100원의 상금을 받게 된다.
    모두 다른 눈이 나오는 경우에는 (그 중 가장 큰 눈)×100원의 상금을 받게 된다.
    
    [Why]이렇게 푼 이유
    ->주사위의 개수가 늘어날 때마다 if문 더 늘리면 힘드니까... 주사위의 개수가 달라질 경우 변경적용을 쉽게하는 알고리즘을 써보고 싶었음.
    --> 이러려면 주사위 개수만큼 입력 받게 수정해 야함, 주사위 갯수 만큼 roll배열수가 증가해야함.
    ---> 왜 반복입력 안 했나요? "풀다가 까먹음. 일요일인데 출퇴근해서 제정신이 아님. 그만하고 싶음..."

--- Project1/source1.cpp
+++ Project1/source1.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 
 int main() {
-	int A, B, C;
-	std::cin >> A >> B >> C;
-	B += C;
-	A += (B / 60);
-	B %= 60;
-	A %= 24;
-	std::cout << A << " " << B;
+	int dice[6] = { 0 };
+	int roll[3];
+	std::cin >> roll[0] >> roll[1] >> roll[2];
+	for (int i = 1; i < 7; i++) {
+		for (int j = 0; j < 3; j++)
+		{
+			if (roll[j] == i)
+				dice[i-1]++;
+		}
+	}
+	int result;
+	for (int i = 0; i < 6; i++)
+	{
+		if (dice[i] == 3) {
+			result = 10000 + (i+1) * 1000;;
+			std::cout << result;
+			return 0;
+		}
+		if (dice[i] == 2) {
+			result = 1000+(i+1)*100;
+			std::cout << result;
+			return 0;
+		}
+	}
+	for (int i = 5; i > 0; i--)
+	{
+		if (dice[i] == 1) {
+			result = (i+1) * 100;
+			std::cout << result;
+			return 0;
+		}
+	}
 }
