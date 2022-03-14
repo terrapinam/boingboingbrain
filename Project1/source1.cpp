3bd6703d381ec12dc0f5a92bbb006088f595772c
terrapinam <terrapinam77@gmail.com>
2022-03-14 20:17:31 +0900

    어제 푼거(주사위문제) 동적메모리_할당 & 배열크기_입력받기 추가
    1~6 범위 체크하는 조건문은 그냥 안 넣음.

--- Project1/source1.cpp
+++ Project1/source1.cpp
@@ -2,8 +2,15 @@
 
 int main() {
 	int dice[6] = { 0 };
-	int roll[3];
-	std::cin >> roll[0] >> roll[1] >> roll[2];
+	int size;
+	std::cout << "Enter the number of dice to roll : ";
+	std::cin >> size;
+	int* roll = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << "roll[" << i << "] Enter a value (1~6) : ";
+		std::cin >> roll[i];
+	}
 	for (int i = 1; i < 7; i++) {
 		for (int j = 0; j < 3; j++)
 		{
