d6698a782175bcc081e0cfd24a66efddb76eddfa
terrapinam <terrapinam77@gmail.com>
2022-04-07 22:21:57 +0900

    multiplication table

--- Project2/second22.cpp
+++ Project2/second22.cpp
@@ -1,2 +1,13 @@
-//test
-//I do my mistake..
+#include <iostream>
+
+//OMG I haven't solved for too long
+
+int main() {
+	int N;
+	std::cin >> N;
+	for (int i = 1; i <= 9; i++)
+	{
+		std::cout << N << " * " << i << " = " << (N * i)<<std::endl;
+	}
+	return 0;
+}
